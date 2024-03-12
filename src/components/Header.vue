a9d1974d87a8dcd6a3b83972074f37b195dee439
brunnaserafina <brunnaserafina@gmail.com>
2024-03-12 20:10:59 -0300

    style: Adicionado Header

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+<template>
+  <div class="header">
+    <h1>BuzzQuizz</h1>
+  </div>
+</template>
+<style scoped>
+.header {
+  height: 70px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #ec362d;
+  box-shadow: 0px 0px 10px black;
+}
+
+h1 {
+  color: #ffffff;
+}
+</style>
